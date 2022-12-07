d5675f253258bbd0aa779bcf2dd0ef289daec10a
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2022-12-07 14:39:48 +0530

    backend api html and css

--- index.js
+++ index.js
@@ -12,10 +12,31 @@ app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
+var backendHtml = '\
+        <html>\
+        <link rel="preconnect" href="https://fonts.googleapis.com">\
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">\
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">\
+            <body style="background-color:black; margin:0px; padding:0px; font-family: Josefin Sans, sans-serif;;\">\
+                <div style="display: flex;\
+                    justify-content: center;\
+                    align-items: center;\
+                    height: 100vh;\
+                    margin:0px; padding:0px;\
+                    width: 100vw;\
+                    font-size: 50px;\
+                    color: white;">\
+                        <h1>pu</h1> <br> \
+                        <h1>Meet</h1> \
+                        &nbsp;<p style="font-size:50px;">API</p>\
+                </div>\
+            </body>\
+        </html>\
+';
 app.get("/", (req, res)=>{
-    res.send("Hello there");
+    res.send(backendHtml);
 })
-
 app.post("/register/student", async(req, res)=>{
     const {
         username, 
